5d61209ad96c5fcd87783f30e252c0335d9a8e6b
Kacper Kupczak <kacper.kupczak@hycom.pl>
2024-09-02 13:52:56 +0200

    Cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,9 @@
 "use client";
 
-import { useMachine } from "@xstate/react";
-
-import { waterMachine } from "../../machines/waterMachine";
-
 export default function Home() {
-  const [state, send] = useMachine(waterMachine);
-
   return (
     <main>
       <h1>XState works!</h1>
-
-      <p>
-        Initial state: <strong>{JSON.stringify(state.value)}</strong>.
-      </p>
-
-      <button
-        type="button"
-        onClick={() =>
-          send({
-            type: "HEAT",
-          })
-        }
-      >
-        HEAT
-      </button>
-
-      <button
-        type="button"
-        onClick={() =>
-          send({
-            type: "FREEZE",
-          })
-        }
-      >
-        FREEZE
-      </button>
     </main>
   );
 }
