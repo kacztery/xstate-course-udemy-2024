a7b30128d6fd081772aa1daa2e2d0f2e274ff036
Kacper Kupczak <kacper.kupczak@orange.com>
2024-09-19 11:29:48 +0200

    Add Fetch machine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,30 @@
 "use client";
 
-import { useRef } from "react";
 import { useMachine } from "@xstate/react";
-
-import { videoPlayerMachine } from "../../machines/videoPlayerMachine";
+import { fetchMachine } from "../../machines/fetchMachine";
 
 export default function Home() {
-  const videoRef = useRef(null);
-
-  const machine = videoPlayerMachine(videoRef, { autoplay: true });
-  const [state, send] = useMachine(machine);
-
-  const playingVideo = () =>
-    send({
-      type: "STOP",
-    });
+  const [state, send] = useMachine(fetchMachine);
 
   return (
     <main>
-      <h1>XState works! Video Player Machine!</h1>
+      <h1>XState works! Fetch Machine!</h1>
 
-      <div>
-        <video
-          width="320"
-          src="https://v3.cdnpk.net/videvo_files/video/free/2014-06/large_preview/Blue_Sky_and_Clouds_Timelapse_0892__Videvo.mp4?token=exp=1725355308~hmac=2ce87648cf26206e237a51590a80f3b3ad88f6234f3b30ddd0c3566a31dab7eb"
-          muted
-          ref={videoRef}
-        />
-      </div>
+      {state.matches("pending") && <p>Fetching data...</p>}
 
-      <button
-        type="button"
-        onClick={() =>
-          send({
-            type: "PLAY",
-          })
-        }
-      >
-        PLAY
-      </button>
+      {state.matches("success") && (
+        <ul>
+          {state.context.data.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      )}
 
-      <button type="button" onClick={playingVideo}>
-        STOP
+      {state.matches("error") && <p>Error...</p>}
+
+      <br />
+      <button type="button" onClick={() => send("FETCH")}>
+        Fetch
       </button>
     </main>
   );
